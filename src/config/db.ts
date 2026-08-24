da2fe7656af9da8d04528deda2999637ef98b383
Sarin <joshisarinatwork@gmail.com>
2026-08-24 11:55:55 +0545

    Initial commit

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import env from "./env";
+
+export async function connectDB() {
+  try {
+    if (!env.DB_URI) throw new Error("Connection failed!");
+    await mongoose.connect(env.DB_URI);
+    console.log("DB connected successfully!");
+  } catch (error) {
+    console.log("DB connection failed!", error);
+  }
+}
---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+const env = {
+  DB_URI: process.env.DB_URI,
+  PORT: process.env.PORT,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFREESH_TOKEN_EXPIRES: process.env.REFRESH_TOKEN_EXPIRES,
+};
+
+for (const [key, value] of Object.entries(env)) {
+  if (!value) {
+    throw new Error(`MISSING ENVIRONMENT VARIABLE: ${key}`);
+  }
+}
+
+export default env;
