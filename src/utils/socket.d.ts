1b8570ffe814496b1ab468be8cdbab0cbdf97914
Sarin <joshisarinatwork@gmail.com>
2026-09-15 10:48:38 +0545

    deployment changes

---
+++ src/utils/socket.d.ts
@@ -0,0 +1,11 @@
+import { Socket } from "socket.io";
+
+declare module "socket.io" {
+  interface Socket {
+    user: {
+      userId: string;
+      email: string;
+      name: string;
+    };
+  }
+}
